a15a8e5cd3db469bb0b0b8b7301ed76c6497fe42
Oliver Jones <oj17@hotmail.co.uk>
2018-07-08 22:48:27 +0100

    JDBC work

---
+++ src/ch10JDBC/JDBCtest.java
@@ -0,0 +1,34 @@
+package ch10JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBCtest {
+
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:derby:demo;create=true";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate("CREATE TABLE species ("
+                    + "id INTEGER PRIMARY KEY,"
+                    + "name VARCHAR(255), "
+                    + "num_acres DECIMAL)");
+            stmt.executeUpdate("CREATE TABLE animal ("
+                    + "id INTEGER PRIMARY KEY, "
+                    + "Species_id integer, "
+                    + "name VARCHAR(255), "
+                    + "date_born TIMESTAMP");
+            stmt.executeUpdate("INSERT INTO species VALUES (1,'Honey Badger', 7.7)");
+            stmt.executeUpdate("INSERT INTO species VALUES (2, 'Zebra',1.2)");
+            stmt.executeUpdate("INSERT INTO animal VALUES (1, 1, 'Elsa', '2001−05−06 02:15')");
+            stmt.executeUpdate("INSERT INTO animal VALUES (2, 2, 'Zelda', '2002−08−15 09:12')");
+            stmt.executeUpdate("INSERT INTO animal VALUES (3, 1, 'Ester', '2002−09−09 10:36')");
+            stmt.executeUpdate("INSERT INTO animal VALUES (4, 1, 'Eddie', '2010−06−08 01:24')");
+            stmt.executeUpdate("INSERT INTO animal VALUES (5, 2, 'Zoe', '2005−11−12 03:44')");
+        }
+
+    }
+
+}
