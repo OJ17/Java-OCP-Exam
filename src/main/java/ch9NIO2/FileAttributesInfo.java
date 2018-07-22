5fc8e6d02edaf70f8249c13050b343d6d5ec3d5c
Oliver Jones <oj17@hotmail.co.uk>
2018-07-22 14:58:18 +0100

    completed NIO work

--- src/main/java/ch9NIO2/FileAttributesInfo.java
+++ src/main/java/ch9NIO2/FileAttributesInfo.java
@@ -1,4 +1,67 @@
 package ch9NIO2;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.UserPrincipal;
+import java.time.Instant;
+
 public class FileAttributesInfo {
+
+    public static void main(String[] args) {
+
+        try {
+            Path pathRezz = Paths.get("/have/no/Fear.txt");
+            Path pathRezz2 = Paths.get("/have/no/mind/Wolf.txt");
+            Path p1 = Paths.get("/users/dmt/Documents/JavaOCP/IOdemo.txt");
+            // this method checks if a file is hidden or not
+            System.out.println(Files.isHidden(pathRezz));
+            // does the file have read access?
+            System.out.println(Files.isReadable(pathRezz));
+            // is the file executable?
+            System.out.println(Files.isExecutable(pathRezz));
+            // Files.size() reads and returns the file length in byes
+            System.out.println(Files.size(
+                    p1)
+            );
+            // getLastModifiedtime()
+            System.out.println(Files.getLastModifiedTime(p1));
+            // setLastModifiedTime()
+            Instant is = Instant.now();
+            FileTime ft = FileTime.from(is);
+            // this changes the file object on the file system,
+            // and sets the last modified time to today
+            System.out.println(Files.setLastModifiedTime(p1,ft));
+            System.out.println(Files.getLastModifiedTime(p1));
+
+            //getOwner()
+            System.out.println(Files.getOwner(p1));
+
+            // setOwner()
+            UserPrincipal owner = FileSystems.getDefault()
+                    .getUserPrincipalLookupService().lookupPrincipalByName("dmt");
+            System.out.println(Files.setOwner(p1,owner));
+
+            // Basic file attributes contains a listof the basic attributes of a file
+            // e.g. name, isDirectory, lastModified, creationTime, etc.
+            BasicFileAttributes basicFileAttributes = Files.readAttributes(p1, BasicFileAttributes.class);
+            System.out.println("Creation time: " + basicFileAttributes.creationTime());
+            System.out.println("Is regulat file: " + basicFileAttributes.isRegularFile());
+
+            // BasicFileAtrributeView - this is used to modify a file's set of date time
+            // values. You cannot modify other attributes directly
+            BasicFileAttributeView view = Files.getFileAttributeView(p1,
+                    BasicFileAttributeView.class);
+
+
+
+
+        } catch (IOException e) {
+
+        }
+
+
+    }
 }
--- src/main/java/ch9NIO2/StreamsForFiles.java
+++ src/main/java/ch9NIO2/StreamsForFiles.java
@@ -1,6 +1,65 @@
 package ch9NIO2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class StreamsForFiles {
 
 
+    public static void main(String[] args) {
+        // walking a directory is a recursive file search that is performed by streams\
+        // this code iterates over a file, and prints out the path of all the files
+        // that end with .java
+        Path p1 = Paths.get("");
+        try {
+            Files.walk(p1)
+                    .filter(p -> p.toString().endsWith(".java"))
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+
+        }
+
+        System.out.println("\n\n\n");
+
+        // find
+        Path p2 = Paths.get("");
+        long dateFilter = 1420070400000l;
+        try {
+            Stream<Path> stream = Files.find(p2, 10,
+                    (p, a) -> p.toString().endsWith(".java")
+                            && a.lastModifiedTime().toMillis() > dateFilter);
+            stream.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+        }
+
+        System.out.println("\n\n\n");
+
+        // list - this code lists through a directory listing all the files
+        // that are not directories it contains
+        Path p3 = Paths.get("");
+        try {
+            Files.list(p2)
+                    .filter(p -> !Files.isDirectory(p))
+                    .map(p -> p.toAbsolutePath())
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+
+        }
+
+        // lines - printing file contents. You can use the usual collectors / filters
+        // for processing what lines get printed / manipulated
+        Path p4 = Paths.get("");
+        try {
+            Files.lines(p4).forEach(System.out::println);
+        } catch (IOException e) {
+
+        }
+
+
+    }
 }
